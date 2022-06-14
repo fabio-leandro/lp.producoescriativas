f3cbd10529c3c45420bdf38e17f015671e8d6569
Fabio Leandro <fabiolpsolucoes@gmail.com>
2022-06-14 10:45:21 -0300

    Script para esconder collapse

---
+++ scripties/script.js
@@ -0,0 +1,10 @@
+function esconder(){
+    var lista = document.querySelector('#navbar-items ul');
+    lista.setAttribute('hidden','true');
+    
+}
+
+function mostrar(){
+    var lista = document.querySelector('#navbar-items ul');
+    lista.removeAttribute('hidden');
+}
